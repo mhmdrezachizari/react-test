ceadfcd4716399e26686fc5e88e65f4176b1c152
mohammadrezachizari <mhmdrezachizari@gmail.com>
2024-01-23 16:18:38 +0330

    new

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import styles from './new.module.css'; 
 import { BsCashCoin } from "react-icons/bs";
+import { useState } from 'react';
 function card({data}) {
+  const [resultbuy, setresultbuy] = useState([])
+   const submitHandler= (e) =>{
+    console.log(e);
+    return
+  }
   return (
-<div className={ styles.div1}>
+<div className={styles.div1}>
     <h5 className={styles.title1}>{data.title}</h5>
     <h6 className={styles.price1}>price is : {data.price}  <BsCashCoin /> </h6>
     <img src={data.image} alt="" className={styles.sty}/>
+    <button onClick={submitHandler}>buy</button>
+    <button>delete</button>
 </div>
 )
 }
